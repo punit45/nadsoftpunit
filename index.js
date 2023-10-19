f68e711c5b322835eeb79226950995b75391ff39
punit45 <ipunit96@gmail.com>
2023-10-19 14:03:31 +0530

    nadsoft assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function handleChange() {
+    prompt ("Are sure you want to book your call now??", "");
+}
+
+
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
